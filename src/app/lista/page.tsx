0e03eaab9f6908637f8a25bfb493605310bb3954
Johnny <johnny12mf@gmail.com>
2025-02-21 19:13:36 -0300

    Tela Inicial - TecWeb

---
+++ src/app/lista/page.tsx
@@ -0,0 +1,10 @@
+export default function Lista(){
+    return (
+        <div>
+          <h1>Lista</h1>
+            <ul>
+                <li>Lista</li>
+            </ul>
+        </div>
+    );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,100 +2,68 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-2 sm:p-1 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-6 row-start-2 items-center">
         <Image
           className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
+          src="/ufc_out.png"
+          alt="UFC logo"
+          width={360}
+          height={75}
           priority
         />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+        <ul className="text-center font-[family-name:var(--font-geist-mono)]">
+          <li>Cadastro de Atividades Acadêmicas</li>
+          <li>Monitoramento dos Participantes</li>
+        </ul>
+
+        <form className="p-4 rounded-xl border border-solid border-black/[.08] dark:border-white/[.145]">
+          <label className="font-[family-name:var(--font-geist-mono)]">Nome da atividade: </label>
+          <input type="text" className="mb-4 text-black w-full rounded-md pl-2 pr-2" name="nome"></input>
+          <br/>
+          <label className="font-[family-name:var(--font-geist-mono)]">Responsável: </label>
+          <input type="text" className="mb-4 text-black w-full rounded-md pl-2 pr-2" name="resp"></input>
+          <br/>
+          <label className="font-[family-name:var(--font-geist-mono)]">Data: </label>
+          <input type="date" className="mb-4 text-black rounded-md pl-2 pr-2" name="data"></input>
+          <br/>
+          <label className="font-[family-name:var(--font-geist-mono)]">Descrição: </label>
+          <textarea name="desc" rows={2} cols={40}  className="text-black w-full rounded-md pl-2 pr-2"></textarea>
+        </form>
+
+       
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+
+          <button
+            className="rounded-xl border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
+            
           >
             <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
+              src="/send.png"
+              alt="Icone Cadastrar"
               width={20}
               height={20}
             />
-            Deploy now
-          </a>
+            Cadastrar
+          </button>
           <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            className="rounded-xl border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center gap-2 hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
+            href="lista"
             target="_blank"
-            rel="noopener noreferrer"
           >
-            Read our docs
+            <Image
+              className="dark:invert"
+              src="/list.png"
+              alt="Icone Lista"
+              width={20}
+              height={20}
+            />
+            Ver Lista
           </a>
         </div>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      
     </div>
   );
 }
